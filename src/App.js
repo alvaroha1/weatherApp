5588dfed82a25fabfde62b8d916c8c7e58aa3b4b
Alvaro Hernandez <alvaroha@Alvaros-MacBook-Air.local>
2019-01-12 22:12:36 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to my WeatherApp</h1>
+        <div className="citySelector">
+          <h4>Select Your City:</h4>
+          <button>Amsterdam</button>
+          <button>New York</button>
+          <button>Moscow</button>
+        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
